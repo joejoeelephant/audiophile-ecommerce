fa7829f160c32f37ce18e1cb4d8c4beeed2d12f4
joejoe <joejoeelephant@gmail.com>
2024-02-02 22:19:34 +0800

    fix type problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { navList } from '@/lib/navList';
 
 export function middleware(req: NextRequest) {
+  if(req.nextUrl.pathname === '/product') {
+    return NextResponse.rewrite(new URL('/product/headphones', req.url));
+  }
   if (req.nextUrl.pathname.startsWith('/product')) {
       const slug = req.nextUrl.pathname.split('/')[2]; // Get the slug from URL
       const slugValid = navList.find(item => item.title === slug); // Validation logic
